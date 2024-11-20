2f8ea541741b37b6c5549fab7282c9880ba81aa3
sivachandran06 <sivachandran997@gamil.com>
2024-11-20 13:52:57 +0530

    final

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+// Utility function to read the data from the database (data.json)
 async function getDataFromDatabase() {
   return new Promise((resolve, reject) => {
     fs.readFile('src/data/data.json', (err, data) => {
@@ -12,24 +13,33 @@ async function getDataFromDatabase() {
   });
 }
 
-async function saveDataToDatabase(data) {
-  return new Promise((resolve, reject) => {
-    const jsonData = JSON.stringify(data);
-    fs.writeFile('src/data/data.json', jsonData, (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
 // Level 1: Get City Weather Data by Name
 async function getWeatherDataByName(cityName) {
-  
-}
+  try {
+    // Get weather data from the database (data.json)
+    const data = await getDataFromDatabase();
+    
+    // Find the city data based on the city name
+    const cityData = data.find(city => city.city.toLowerCase() === cityName.toLowerCase());
+    
+    if (!cityData) {
+      throw new Error('City not found');
+    }
 
+    // Format the weather data to return
+    const weatherData = {
+      city: cityData.city,
+      temperature: cityData.weather.temperature,
+      humidity: cityData.weather.humidity,
+      windSpeed: cityData.weather.windSpeed,
+      conditions: cityData.weather.conditions
+    };
+
+    return weatherData;
+  } catch (error) {
+    throw new Error('City not found');
+  }
+}
 
 module.exports = {
   getWeatherDataByName
